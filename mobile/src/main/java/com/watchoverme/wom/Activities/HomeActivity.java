ece40380f34db7c3349226f441e59da13f3701ca
waqas <waqax94@gmail.com>
2018-12-08 21:19:15 +1100

    More

--- mobile/src/main/java/com/watchoverme/wom/Activities/HomeActivity.java
+++ mobile/src/main/java/com/watchoverme/wom/Activities/HomeActivity.java
@@ -3,20 +3,49 @@ package com.watchoverme.wom.Activities;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.watchoverme.wom.Adapters.ViewPagerAdapter;
+import com.watchoverme.wom.Fragments.ContactFragment;
+import com.watchoverme.wom.Fragments.HomeFragment;
+import com.watchoverme.wom.Fragments.NotificationFragment;
 import com.watchoverme.wom.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
+    HomeFragment homeFragment;
+    NotificationFragment notificationFragment;
+    ContactFragment contactFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }
 
+        tabLayout = (TabLayout) findViewById(R.id.menu_tabs);
+        viewPager = (ViewPager) findViewById(R.id.menu_pages);
+
+        homeFragment = new HomeFragment();
+        notificationFragment = new NotificationFragment();
+        contactFragment = new ContactFragment();
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        viewPagerAdapter.addFragments(notificationFragment,"Notifications");
+        viewPagerAdapter.addFragments(homeFragment,"Home");
+        viewPagerAdapter.addFragments(contactFragment,"Contacts");
+
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
 }
---
+++ mobile/src/main/java/com/watchoverme/wom/Adapters/ViewPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.watchoverme.wom.Adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    ArrayList<Fragment> fragments = new ArrayList<>();
+    ArrayList<String> tabTitles = new ArrayList<>();
+
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragments(Fragment fragment,String title){
+        this.fragments.add(fragment);
+        this.tabTitles.add(title);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles.get(position);
+    }
+}
